68fdc32ab135786ea8fb69999c776c7f6a72a3d1
iamdanishm <mansuridanish14@gmail.com>
2026-09-25 21:59:36 +0530

    feat: add allowed dev origins to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    '192.168.0.101',
+    '192.168.0.*',
+    '192.168.1.*',
+    '*.local',
+  ],
   images: {
     remotePatterns: [
       {
